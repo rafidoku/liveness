a06dbf2fa0a1e6b8a03997af49518fdb3401b6e8
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-04-28 13:49:55 +0700

    add cameraview

--- Sources/LivenessCamera/View/LivenessCameraView.swift
+++ Sources/LivenessCamera/View/LivenessCameraView.swift
@@ -1,6 +1,42 @@
 import UIKit
+import AVFoundation
 
 public class LivenessCameraViewController: UIViewController {
+    
+    @IBOutlet var cameraView: UIView!
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCaptureStillImageOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let devices = AVCaptureDevice.devices(for: .video)
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.front {
+                do {
+                    let input = try AVCaptureDeviceInput(device: device as! AVCaptureDevice)
+                    if captureSession.canAddInput(input) {
+                        captureSession.addInput(input)
+                        sessionOutput.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+                        
+                        if captureSession.canAddOutput(sessionOutput) {
+                            captureSession.addOutput(sessionOutput)
+                            captureSession.startRunning()
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                            previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                            cameraView.layer.addSublayer(previewLayer)
+                            previewLayer.position= CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                            previewLayer.bounds = cameraView.frame
+                        }
+                    }
+                } catch let error {
+                    print("error device ", error.localizedDescription)
+                }
+            }
+        }
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
     }
