4adc2e80605d7c31e802cbbe203137cecc8a8877
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-04-28 10:47:00 +0700

    add example project
    add example project to package folder

---
+++ LivenessPackageExample/Shared/LivenessPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivenessPackageExampleApp.swift
+//  Shared
+//
+//  Created by DOKU IT on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LivenessPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
