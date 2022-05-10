da2e0a8638649603eff9b3a774db59070ca6cae2
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-05-10 15:15:18 +0700

    degree setting

--- Sources/LivenessCamera/View/LivenessCameraView.swift
+++ Sources/LivenessCamera/View/LivenessCameraView.swift
@@ -21,8 +21,8 @@ public class LivenessCameraViewController: UIViewController {
         borderView.layer.cornerRadius = borderView.frame.width / 2
         let path = UIBezierPath(arcCenter: CGPoint(x: self.borderView.frame.size.width/2, y: self.borderView.frame.size.height/2),
                                 radius: self.borderView.frame.size.height/2,
-                                startAngle: CGFloat(0.0).toRadians(),
-                                endAngle: CGFloat(24.0).toRadians(),
+                                startAngle: CGFloat(270.0).toRadians(),
+                                endAngle: CGFloat(180.0).toRadians(),
                                 clockwise: true)
         let shape = CAShapeLayer()
         shape.path = path.cgPath
