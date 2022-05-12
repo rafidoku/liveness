fcf3d69e2057fbfa4949a3a051da572cf4a99add
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-05-12 16:01:45 +0700

    big fizes

--- Sources/LivenessCamera/View/LivenessCameraView.swift
+++ Sources/LivenessCamera/View/LivenessCameraView.swift
@@ -76,7 +76,7 @@ public class LivenessCameraViewController: UIViewController, AVCapturePhotoCaptu
     
     private func animateProgress(progress: Int) {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.toValue = progress / 15.0
+        basicAnimation.toValue = CGFloat(progress) / 15.0
         basicAnimation.duration = 2
         basicAnimation.fillMode = .forwards
         basicAnimation.isRemovedOnCompletion = false
