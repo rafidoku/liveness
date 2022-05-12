3ca25ca03aab41fe852048be5966f6f2ca8b1ef6
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-05-12 16:11:16 +0700

    degree adjust

--- Sources/LivenessCamera/View/LivenessCameraView.swift
+++ Sources/LivenessCamera/View/LivenessCameraView.swift
@@ -77,7 +77,7 @@ public class LivenessCameraViewController: UIViewController, AVCapturePhotoCaptu
     
     private func animateProgress(progress: Int) {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        progreeTaken += 0.67
+        progreeTaken += CGFloat(progress) / 15.0
         basicAnimation.toValue =  progreeTaken
         shape.strokeEnd = progreeTaken
         basicAnimation.duration = 2
