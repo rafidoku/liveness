7c1b49b961ae292d38be53bccaa140528135e345
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-05-13 14:36:41 +0700

    border

--- Sources/LivenessCamera/View/LivenessCameraView.swift
+++ Sources/LivenessCamera/View/LivenessCameraView.swift
@@ -78,7 +78,7 @@ public class LivenessCameraViewController: UIViewController, AVCapturePhotoCaptu
     
     private func animateProgress(progress: Int) {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        progreeTaken += 0.05
+        progreeTaken += 0.055
         basicAnimation.toValue =  progreeTaken
         shape.strokeEnd = progreeTaken
         basicAnimation.duration = 0.5
